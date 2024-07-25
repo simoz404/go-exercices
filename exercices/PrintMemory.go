a0877a56865bde03e05a357a426fe74507aa519d
BAKHOUCH MOHAMED <62752170+simoz404@users.noreply.github.com>
2024-07-25 12:50:15 +0100

    PrintMemory.go

---
+++ exercices/PrintMemory.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func PrintMemory(arr [10]byte) {
+	dec := "0123456789abcdef"
+	var out string
+	for i := 0; i < len(arr); i++ {
+		div := arr[i]/16
+		mod := arr[i]%16
+		s := string(dec[div])+string(dec[mod])
+		out += s + " "
+	}
+	fmt.Print(out)
+}
+
+func main() {
+	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
+}
