49e9761ca460e4f6f6e1e3b4262ac5a3b2381d7a
mbakhouc <bakhouch2019@gmail.com>
2024-07-29 18:07:53 +0100

    slice

--- main.go
+++ main.go
@@ -4,13 +4,45 @@ import (
 	exercice "go-exercices/exercices"
 
 	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/lib/chars"
+	"github.com/01-edu/go-tests/lib/random"
 	"github.com/01-edu/go-tests/solutions"
 )
 
 func main() {
-	table := []string{"1234556789", "QKplq%QSw", "", "Kimetsu no Yaiba", "Z", "email123@live.fr", "8595485-52", "-552", "abc", "w58tw7474abc", "fifa world cup `2022`"}
+	elems := [][]interface{}{
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			1,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			-3,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			2, 4,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			-2, -1,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			2, 0,
+		},
+	}
 
-	for _, s := range table {
-		challenge.Function("ThirdTimeIsACharm", exercice.ThirdTimeIsACharm, solutions.ThirdTimeIsACharm, s)
+	s := random.StrSlice(chars.Words)
+
+	elems = append(elems, []interface{}{s, -len(s) - 10, -len(s) - 5})
+
+	for i := 0; i < 3; i++ {
+		s = random.StrSlice(chars.Words)
+		elems = append(elems, []interface{}{s, random.IntBetween(-len(s)-10, len(s)+10), random.IntBetween(-len(s)-8, len(s)+10)})
+	}
+
+	for _, a := range elems {
+		challenge.Function("Slice", exercice.Slice, solutions.Slice, a...)
 	}
 }
